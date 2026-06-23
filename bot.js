568888cf0581028f6d6f204a826dfc115cb79c18
esmaelyassin58-cell <esmaelyassin58@gmail.com>
2026-06-24 02:11:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// index.html ለሚኒ አፑ ማዘጋጀት
+// index.html ዝግጁ ማድረግ
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// ሰርቨር ማስጀመር (ምንም አይነት ውስብስብ ጥቅስ የሌለው ቀላል መስመር)
+// ሰርቨር ማስጀመር
 app.listen(PORT, () => {
-  console.log("Server is running successfully...");
+  console.log("Server is running successfully");
 });
 
 // የቴሌግራም ቦት ማስጀመር
@@ -22,46 +22,45 @@ const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || '8674688097:AAEJfKxw8
 
 bot.start((ctx) => {
   ctx.reply(
-🎉 እንኳን ወደ Bingo7 በደህና መጡ!
-
-/register - ምዝገባ
-/play - ጨዋታ
-/balance - ቀሪ ሂሳብ
-/deposit - ገንዘብ መጨመር
-/withdraw - ገንዘብ ማውጣት
-/support - ድጋፍ
+    "Welcome to Bingo7!\n\n" +
+    "/register - ምዝገባ\n" +
+    "/play - ጨዋታ\n" +
+    "/balance - ቀሪ ሂሳብ\n" +
+    "/deposit - ገንዘብ መጨመር\n" +
+    "/withdraw - ገንዘብ ማውጣት\n" +
+    "/support - ድጋፍ"
   );
 });
 
 bot.command('register', (ctx) => {
-  ctx.reply('📝 ምዝገባ በቅርቡ ይጀምራል');
+  ctx.reply('ምዝገባ በቅርቡ ይጀምራል');
 });
 
 bot.command('play', (ctx) => {
-  ctx.reply('🎮 ጨዋታው በቅርቡ ይከፈታል');
+  ctx.reply('ጨዋታው በቅርቡ ይከፈታል');
 });
 
 bot.command('balance', (ctx) => {
-  ctx.reply('💰 ቀሪ ሂሳብ: 0 ETB');
+  ctx.reply('ቀሪ ሂሳብ: 0 ETB');
 });
 
 bot.command('deposit', (ctx) => {
-  ctx.reply('💵 ገንዘብ ለመጨመር የክፍያ መንገድ በቅርቡ ይታከላል');
+  ctx.reply('ገንዘብ ለመጨመር የክፍያ መንገድ በቅርቡ ይታከላል');
 });
 
 bot.command('withdraw', (ctx) => {
-  ctx.reply('💸 ገንዘብ ለማውጣት በቅርቡ ይገኛል');
+  ctx.reply('ገንዘብ ለማውጣት በቅርቡ ይገኛል');
 });
 
 bot.command('support', (ctx) => {
-  ctx.reply('☎️ ለድጋፍ: @your_support_username');
+  ctx.reply('ለድጋፍ: @your_support_username');
 });
 
-// ቦቱን ማንቀሳቀስ
+// ቦቱን ማስነሳት
 bot.launch().then(() => {
-  console.log('✅ Bingo7 Bot Started');
+  console.log('Bingo7 Bot Started');
 }).catch((err) => {
-  console.error('Bot launch error:', err);
+  console.error('Bot error:', err);
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
