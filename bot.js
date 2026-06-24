8bcd9f64c84a0054a010b7b060408d87506f5d8a
esmaelyassin58-cell <esmaelyassin58@gmail.com>
2026-06-24 21:31:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// index.html ዝግጁ ማድረግ
+// የሪንደር አፕሊኬሽን ሊንክ (ሚኒ አፑ የሚከፈትበት ዋና አድራሻ)
+const RENDER_URL = 'https://bingo7-miniapp.onrender.com';
+
+// 1. የሚኒ አፑን የፊት ገጽ (HTML/CSS/JS) ዝግጁ ማድረግ
 app.use(express.static(path.join(__dirname)));
 
 app.get('/', (req, res) => {
@@ -14,54 +17,45 @@ app.get('/', (req, res) => {
 
 // ሰርቨር ማስጀመር
 app.listen(PORT, () => {
-  console.log("Server is running successfully");
+  console.log(Server is running successfully on port ${PORT});
 });
 
-// የቴሌግራም ቦት ማስጀመር (|| ምልክቶች ተስተካክለዋል)
+// 2. የቴሌግራም ቦት መዋቅር
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN || '8674688097:AAEJfKxw8FLiOU73vLBft2JZM200f6MOfDE');
 
+// ተጠቃሚው /start ሲል የሚኒ አፑን መክፈቻ ቁልፍ (Web App Button) ያሳየዋል
 bot.start((ctx) => {
   ctx.reply(
-    "Welcome to Bingo7!\n\n" +
-    "/register - ምዝገባ\n" +
-    "/play - ጨዋታ\n" +
-    "/balance - ቀሪ ሂሳብ\n" +
-    "/deposit - ገንዘብ መጨመር\n" +
-    "/withdraw - ገንዘብ ማውጣት\n" +
-    "/support - ድጋፍ"
+    "Welcome to Bingo7!\n\nእንኳን ወደ ቢንጎ 7 ጨዋታ በደህና መጡ። ጨዋታውን ለመጀመር ከታች ያለውን 'Play Bingo' የሚለውን ቁልፍ ይጫኑ።",
+    Markup.inlineKeyboard([
+      [Markup.button.webApp('Play Bingo 🎮', RENDER_URL)]
+    ])
   );
 });
 
-bot.command('register', (ctx) => {
-  ctx.reply('ምዝገባ በቅርቡ ይጀምራል');
-});
-
+// ተጠቃሚው /play ሲልም በተመሳሳይ ሚኒ አፑን ይከፍትለታል
 bot.command('play', (ctx) => {
-  ctx.reply('ጨዋታው በቅርቡ ይከፈታል');
+  ctx.reply(
+    "ጨዋታውን ለመጀመር ከታች ያለውን ቁልፍ ይጫኑ፡",
+    Markup.inlineKeyboard([
+      [Markup.button.webApp('Open Mini App 🚀', RENDER_URL)]
+    ])
+  );
 });
 
-bot.command('balance', (ctx) => {
-  ctx.reply('ቀሪ ሂሳብ: 0 ETB');
-});
+// ሌሎች መደበኛ የቦት ትዕዛዞች
+bot.command('register', (ctx) => { ctx.reply('ምዝገባ በቅርቡ ይጀምራል'); });
+bot.command('balance', (ctx) => { ctx.reply('ቀሪ ሂሳብ: 0 ETB'); });
+bot.command('deposit', (ctx) => { ctx.reply('ገንዘብ ለመጨመር የክፍያ መንገድ በቅርቡ ይታከላል'); });
+bot.command('withdraw', (ctx) => { ctx.reply('ገንዘብ ለማውጣት በቅርቡ ይገኛል'); });
+bot.command('support', (ctx) => { ctx.reply('ለድጋፍ: @your_support_username'); });
 
-bot.command('deposit', (ctx) => {
-  ctx.reply('ገንዘብ ለመጨመር የክፍያ መንገድ በቅርቡ ይታከላል');
-});
-
-bot.command('withdraw', (ctx) => {
-  ctx.reply('ገንዘብ ለማውጣት በቅርቡ ይገኛል');
-});
-
-bot.command('support', (ctx) => {
-  ctx.reply('ለድጋፍ: @your_support_username');
-});
-
-// ቦቱን ማስነሳት (የድሮ ግጭቶችን የሚያጠፋ ማስተካከያ ተጨምሯል)
+// 3. ቦቱን በ Long Polling ማስነሳት (የድሮ ግጭቶችን እያጠፋ ይነሳል)
 bot.launch({
   allowedUpdates: ['message', 'callback_query'],
   dropPendingUpdates: true
 }).then(() => {
-  console.log('Bingo7 Bot Started Successfully');
+  console.log('Bingo7 Bot Started Successfully via Polling');
 }).catch((err) => {
   console.error('Bot error:', err);
 });
